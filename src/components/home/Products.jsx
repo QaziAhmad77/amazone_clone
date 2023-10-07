73296eea79a9c4b5bd1be347a1c8a7fb61ae4c91
Qazi Ahmad <qaziahmad1877@gmail.com>
2023-10-07 14:34:20 +0500

    updated

--- src/components/home/Products.jsx
+++ src/components/home/Products.jsx
@@ -1,5 +1,9 @@
 import { useLoaderData } from 'react-router-dom';
 import StarIcon from '@mui/icons-material/Star';
+import ApiIcon from '@mui/icons-material/Api';
+import ShoppinCartIcon from '@mui/icons-material/ShoppingCart';
+import ArrowCircleRightIcon from '@mui/icons-material/ArrowCircleRight';
+import FavoriteIcon from '@mui/icons-material/Favorite';
 
 const Products = () => {
   const data = useLoaderData();
@@ -17,14 +21,25 @@ const Products = () => {
       {productData.map((item, index) => (
         <div
           key={index}
-          className="bg-white h-auto border-[1px] border-gray-200 py-6 z-30 hover:border-transparent shadow-none hover:shadow-testShadow duration-200 relative flex flex-col gap-4"
+          className="bg-white h-auto border-[1px] border-gray-200 py-8 z-30 hover:border-transparent shadow-none hover:shadow-testShadow duration-200 relative flex flex-col gap-4"
         >
-          <div className="w-full h-auto flex items-center justify-center">
+          <span className="text-xs capitalize italic absolute top-2 right-2 text-gray-500">
+            {item.category}
+          </span>
+          <div className="w-full h-auto flex items-center justify-center relative">
             <img
               className="w-52 h-64 object-contain"
               src={item.image}
               alt="ProductImg"
             />
+            <ul className="w-full h-36 bg-gray-100 absolute bottom-0 flex flex-col items-end justify-center gap-2 font-titleFont px-2 border-l border-r">
+              <li>
+                Compare{' '}
+                <span>
+                  <ApiIcon />{' '}
+                </span>
+              </li>
+            </ul>
           </div>
           <div className="px-4">
             <div className="flex items-center justify-between">
@@ -36,8 +51,10 @@ const Products = () => {
               </p>
             </div>
             <div>
-              <p className="text-sm">{item.description.substring(0, 100)}...</p>
-              <div>
+              <p className="text-sm mb-1">
+                {item.description.substring(0, 100)}...
+              </p>
+              <div className="text-yellow-500">
                 <StarIcon />
                 <StarIcon />
                 <StarIcon />
@@ -45,6 +62,9 @@ const Products = () => {
                 <StarIcon />
               </div>
             </div>
+            <button className="w-full font-titleFont font-medium text-base bg-gradient-to-tr from-yellow-400 to-yellow-200 border hover:from-yellow-300 hover:to-yellow-100 border-yellow-500 hover:border-yellow-700 active:bg-gradient-to-bl active:from-yellow-400 active:to-yellow-500 duration-200 py-1.5 rounded-md mt-3">
+              Add to Cart
+            </button>
           </div>
         </div>
       ))}
